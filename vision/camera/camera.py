38c79408f920b0504044469f630b4bbb35c37848
Wellington Vieira M Castro <wellingtonvcastro@gmail.com>
2018-04-10 14:43:15 -0400

    making some folders and starting camera class

---
+++ vision/camera/camera.py
@@ -0,0 +1,15 @@
+import cv2
+
+class Camera:
+    def __init__(self, device_id, camera_name, lens_correction):
+        self.id = device_id
+        self.camera_name = camera_name
+        self.lens_correction = lens_correction
+
+    def set_frame():
+    	pass
+
+    def load_params():
+    	""" Loads the parameters of the camera from a json file in /parameters according
+    	to the camera_name """
+    	pass
