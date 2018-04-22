0b24c9d8da4b155afdfefcadbf79266864f2e214
Mateus Paiva Matiazzi <mateuspaivamatiazzi1@gmail.com>
2018-04-22 17:05:15 -0400

    Desenho do robô e bola
    Terminada função de desenho do robô e da bola.

--- robot/movement/simulator/simulator.py
+++ robot/movement/simulator/simulator.py
@@ -1,5 +1,6 @@
 from auxiliary import *
 import cv2
+import math
 import numpy as np
 
 # window size 
@@ -19,11 +20,11 @@ BR = (700,560)
 # robot 32X32
 # robot center
 robot = (0,0)
-robotSize = 16
+robotSize = (32, 32)
 
 # ball center
 ball = (0,0)
-ballRadius = 2.1
+ballRadius = 8
 
 def drawArena():
     # horizontal lines
@@ -46,15 +47,17 @@ def drawMarks():
     cv2.circle(img,(400,300), 20, (255,255,255), 1)
 
 def drawRobotPos(pos, vec):
-    angle = angleBetween([0,1], vec)
+    angle = angleBetween([1,0], vec)*180/(math.pi)
     robot = pos
-    arTL = (robot[0]-robotSize, robot[1]-robotSize)
-    arBR = (robot[0]+robotSize, robot[1]+robotSize)
+    rect  = (robot, robotSize, angle)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img,[box], 0, (0, 255, 0), -1)
+    cv2.arrowedLine(img, robot, (robot[0]+32*vec[0], robot[1]+32*vec[1]), (0,0,255), 2)
 
-    rTL = rotate(robot, arTL, 3.14/4)
-    rBR = rotate(robot, arBR, -3.14/4)
-
-    cv2.rectangle(img,rTL,rBR,(0,255,0),1)
+def drawBall(pos):
+    ball = pos
+    cv2.circle(img, ball, ballRadius, (31,136,246), -1)
 
 def initArena():
     drawArena()
@@ -62,6 +65,7 @@ def initArena():
 
 if __name__ == "__main__":
     initArena()
-    drawRobotPos((200,200), [1,1])
+    drawRobotPos((200,200), [-1,-1])
+    drawBall((500,500))
     cv2.imshow('Goalkeeper Simulation',img)
     key = cv2.waitKey(0)
