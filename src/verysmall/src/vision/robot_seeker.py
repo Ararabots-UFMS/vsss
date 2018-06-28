59a8c28494bba2ae0b06fe5f7ee4fefeb54530df
wvmcastro <wellingtonvcastro@gmail.com>
2018-06-28 18:45:38 -0400

    mais robot_seeker

--- src/verysmall/src/vision/robot_seeker.py
+++ src/verysmall/src/vision/robot_seeker.py
@@ -1,15 +1,21 @@
+import numpy as np
+import cv2
+
 class Robot:
     def __init__(self):
         self.id = -1
 
         # Stores the (x,y) pos from the rebot
-        self.pos = (None, None)
+        self.pos = np.array([None, None])
 
         # The orientation is stored in radians in relation with the x axis
         self.orientation = None
 
         # Stores the (dx/dt, dy/dt) components
-        self.speed = (None, None)
+        self.speed = np.array([None, None])
+
+        # Saves the time of the last update
+        self.last_update = None
 
 class RobotSeeker:
     # https://docs.opencv.org/master/d9/d8b/tutorial_py_contours_hierarchy.html#gsc.tab=0
@@ -18,6 +24,7 @@ class RobotSeeker:
         pass
 
     def get_contours(self, img):
+        # Takes a binary image as input and return its contours
         im2, cont, h = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         return cont
 
@@ -66,23 +73,60 @@ class RobotSeeker:
 
         return sorted_rectangles[n][0]
 
-    def find_robots(self, robots_list, rectangles, pos=True, direction=False, homeTeam=False):
+    def find_robots(self, robots_list, rectangles, pos=True, direction=False, speed=False, homeTeam=False):
+        # This function fills the robots_list with the info of each robot
+        # If the homeTeam flag is True than it will indentify the home team robots too
 
-        # Find the threshold that separates the big from small rectangles
-        thresh = self.big_small_threshold(rectangles)
+        if homeTeam == True:
+            # Find the threshold that separates the big from small rectangles
+            thresh = self.big_small_threshold(rectangles)
 
-        big_ones = []
-        small_ones = []
+            big_ones = []
+            small_ones = []
+            robots_rects = []
 
-        # Separates the found rectangles in two sets (big ones and small ones)
-        for rect in rectangles:
-            # Checks if the rectangle area is bigger than the threshold
-            if(rect[0] > thresh):
-                big_ones.append(rect)
-            else:
-                small_ones.append(rect)
+            # Separates the found rectangles in two sets (big ones and small ones)
+            for rect in rectangles:
+                # Checks if the rectangle area is bigger than the threshold
+                if(rect[0] > thresh):
+                    big_ones.append(rect)
+                else:
+                    small_ones.append(rect)
 
-        while(len(rectangles) > 0):
+            while(len(big_ones) > 0 and len(small_ones) > 0):
+
+                # Finds the closest small rectangle to the big one
+                dst = np.linalg.norm(np.array(small_ones[:][1]) - np.array(big_ones[0][1]))
+                small_index = np.argmin(dst)
+
+                # Insert the pair in this Auxialiary list
+                robots_rects.append(big_ones[0], small_ones[small_index])
+
+                # Delete the exampples from the samples
+                del big_ones[0]
+                del small_ones[small_index]
+        else:
+            robots_rects = rectangles
+
+        # Now we assume that each element in robots_rects represents a robot
+        for i,r in enumerate(robots_rects):
+
+            if pos == True:
+                if homeTeam == True:
+                    # Calculates the centroind of the two rectangles
+                    # Area [0] and center [1]
+                    x = (r[0][0] * r[0][1][0] + r[1][0] * r[1][1][0]) / (r[0][0] + r[1][0])
+                    y = (r[0][0] * r[0][1][1] + r[1][0] * r[1][1][1]) / (r[0][0] + r[1][0])
+                else:
+                    x,y = r[1]
+
+                robots_list[i].pos = (x,y)
+
+            if direction == True and homeTeam == True:
+
+
+
 
 
 if __name__ == '__main__':
+    pass
