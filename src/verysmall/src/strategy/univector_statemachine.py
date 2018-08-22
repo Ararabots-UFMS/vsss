1150596dd7ce2fa326902598d27f6109d43caa5d
Unknown <marquesnavarezi@hotmail.com>
2018-08-22 12:50:45 -0400

    Calibração para robô 6

--- src/verysmall/src/strategy/univector_statemachine.py
+++ src/verysmall/src/strategy/univector_statemachine.py
@@ -6,7 +6,7 @@ from movement.functions.movement import Movement
 import rospy
 import numpy as np
 
-movement = Movement([40, 0, 0], 10)
+movement = Movement([115, 0.005, 10, 0.05])
 
 class AttackerWithUnivector(StateMachine):
     """Simple attacker with univector field"""
@@ -17,4 +17,6 @@ class AttackerWithUnivector(StateMachine):
     action = univector.to(univector)
 
     def on_action(self, speed, robot_position, robot_vector, robot_speed,  obstacles_position, obstacles_speed,  ball_position):
-        return movement.do_univector(speed, robot_position, [np.cos(robot_vector), np.sin(robot_vector)], [0,0] , obstacles_position, [[0, 0]]*len(obstacles_position), ball_position)
+        rospy.logfatal(str(movement.pid.error))
+        return movement.follow_vector([np.cos(robot_vector), np.sin(robot_vector)], [-1, 0], 130)
+        # return movement.do_univector(speed, robot_position, [np.cos(robot_vector), np.sin(robot_vector)], [0,0] , obstacles_position, [[0, 0]]*len(obstacles_position), ball_position)
