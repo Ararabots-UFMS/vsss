de5402d607f76744248c1bdbfd98db35589bcaa3
Wellington Castro <wellingtonvcastro@gmail.com>
2018-10-19 00:04:16 -0400

    passando a camera como parametro para o get frames do vision utils

--- src/verysmall/src/vision/vision_utils/get_calibration_frames.py
+++ src/verysmall/src/vision/vision_utils/get_calibration_frames.py
@@ -6,12 +6,14 @@ import time
 import numpy as np
 import uuid
 import cv2
+import sys
 
 CAMERA_ID = 0
 PARAMS_FILE = ""
 FRAME_SIZE = (1280, 720)
 
 if __name__ == '__main__':
+    CAMERA_ID = int(sys.argv[1])
     cap = Camera(CAMERA_ID, PARAMS_FILE, False)
     if FRAME_SIZE != ():
         cap.set_frame_size(FRAME_SIZE[0], FRAME_SIZE[1])
@@ -21,7 +23,7 @@ if __name__ == '__main__':
 
         cv2.imshow("get frames window", frame)
         key = cv2.waitKey(1) & 0xFF
-        
+
         if key == ord(' '):
             name = uuid.uuid4().hex + ".jpg"
             cv2.imwrite(name, frame)
