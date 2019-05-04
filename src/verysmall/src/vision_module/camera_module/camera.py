d01bec5b2812966f7882edbc904c6d445262609a
Wellington Castro <wellingtonvcastro@gmail.com>
2019-05-04 17:31:43 -0400

    correcao bugs camera

--- src/verysmall/src/vision_module/camera_module/camera.py
+++ src/verysmall/src/vision_module/camera_module/camera.py
@@ -1,26 +1,24 @@
 from typing import Union
 import cv2
-import os
-import sys
 import numpy as np
-from time import sleep
 from multiprocessing import Queue, Process
-
 from utils.json_handler import JsonHandler
+
 
 # @author Wellington Castro <wvmcastro>
 
 class Camera:
-    def __init__(self, device_id:Union[int, str] = 0,
-                       params_file_name:str = "",
-                       lens_correction:bool = True,
-                       threading:bool = False):
+    def __init__(self, device_id: Union[int, str] = 0,
+                 params_file_name: str = "",
+                 lens_correction: bool = True,
+                 threading: bool = False):
 
         self.id = device_id
         self.lens_correction = lens_correction
         self.params_file_name = params_file_name
         self.threading = threading
         self.capture = cv2.VideoCapture(self.id)
+        self.thread_stopped = True
 
         self.json_handler = JsonHandler()
         self.frame = None
@@ -28,37 +26,42 @@ class Camera:
         if self.params_file_name != "":
             self._load_params()
             self.set_frame_size(self.frame_width, self.frame_height)
-            self.capture_frame = self._capture_frame()
-        else:
             self.capture_frame = self._capture_and_correct_frame
+        else:
+            self.capture_frame = self._capture_frame
+
+        self._calibrate_sensors()
 
         if self.threading == True:
-            self.thread_stopped = True
             self.buffer = Queue(maxsize=1)
             self._start_reading_process()
             self._read = self._threaded_read
         else:
             self._read = self._sequential_read
 
+    def _calibrate_sensors(self):
+        for i in range(10):
+            self.capture_frame()
+
     def _start_reading_process(self) -> None:
         self.thread_stopped = False
         p = Process(target=self._update, args=())
         p.daemon = True
         p.start()
-    
-    def _stop_reading(self) -> None:
+
+    def stop(self) -> None:
         if not self.thread_stopped:
             self.thread_stopped = True
 
     def _update(self) -> None:
-       while not self.thread_stopped:
-           frame = self.capture_frame()
-           self.buffer.put(frame)
+        while not self.thread_stopped:
+            frame = self.capture_frame()
+            self.buffer.put(frame)
 
     def _capture_frame(self) -> np.ndarray:
         _, frame = self.capture.read()
         return frame
-    
+
     def _capture_and_correct_frame(self) -> np.ndarray:
         _, frame = self.capture.read()
         frame = cv2.remap(frame, self.mapx, self.mapy, cv2.INTER_LINEAR)
@@ -73,10 +76,10 @@ class Camera:
         except Exception as e:
             print("ERROR ", end='')
             print("Camera: ", e)
-    
+
     def _threaded_read(self) -> np.ndarray:
         return self.buffer.get(block=True)
-    
+
     def _sequential_read(self) -> np.ndarray:
         return self.capture_frame()
 
@@ -96,11 +99,11 @@ class Camera:
         self.camera_matrix = np.asarray(params['cam_matrix'])
         self.dist_vector = np.asarray(params['dist_vector'])
 
-    def set_frame_size(self, width:int, height:int) -> None:
+    def set_frame_size(self, width: int, height: int) -> None:
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     def __repr__(self) -> str:
         return "Camera(device_id=%r, params_file_name=%r, lens_correction=%r, " \
-                "threading=%r)" % (self.id, self.params_file_name,
-                self.lens_correction, self.threading)
+               "threading=%r)" % (self.id, self.params_file_name,
+                                  self.lens_correction, self.threading)
