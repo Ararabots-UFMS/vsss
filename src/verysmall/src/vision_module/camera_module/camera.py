6733b9ecc3b795e0db14cc31cd54b47be835d62a
Wellington Castro <wcastro@deepblue>
2019-07-10 14:21:50 -0400

    correcao configuracao tamanho frame

--- src/verysmall/src/vision_module/camera_module/camera.py
+++ src/verysmall/src/vision_module/camera_module/camera.py
@@ -1,8 +1,10 @@
 from typing import Union
 import cv2
 import numpy as np
-from multiprocessing import Queue, Process
+from collections import deque
+from threading import Thread, Semaphore
 from utils.json_handler import JsonHandler
+import rospy
 
 
 # @author Wellington Castro <wvmcastro>
@@ -25,7 +27,7 @@ class Camera:
 
         if self.params_file_name != "":
             self._load_params()
-            self.set_frame_size(self.frame_width, self.frame_height)
+            self.set_device(self.frame_width, self.frame_height)
             self.capture_frame = self._capture_and_correct_frame
         else:
             self.capture_frame = self._capture_frame
@@ -33,19 +35,23 @@ class Camera:
         self._calibrate_sensors()
 
         if self.threading == True:
-            self.buffer = Queue(maxsize=1)
+            self._buffer_semaphore = Semaphore(0)
+            self.buffer = deque(maxlen=1)
             self._start_reading_process()
             self._read = self._threaded_read
         else:
             self._read = self._sequential_read
 
     def _calibrate_sensors(self):
-        for i in range(10):
-            self.capture_frame()
+        i = 0
+        while i < 5:
+            ret, _ = self._capture_frame()
+            if ret == True:
+                i += 1
 
     def _start_reading_process(self) -> None:
         self.thread_stopped = False
-        p = Process(target=self._update, args=())
+        p = Thread(target=self._update, args=())
         p.daemon = True
         p.start()
 
@@ -56,11 +62,14 @@ class Camera:
     def _update(self) -> None:
         while not self.thread_stopped:
             frame = self.capture_frame()
-            self.buffer.put(frame)
+            bufferFull = len(self.buffer)
+            self.buffer.append(frame)
+            if not bufferFull:
+                self._buffer_semaphore.release()
 
     def _capture_frame(self) -> np.ndarray:
-        _, frame = self.capture.read()
-        return frame
+        ret, frame = self.capture.read()
+        return ret, frame
 
     def _capture_and_correct_frame(self) -> np.ndarray:
         _, frame = self.capture.read()
@@ -70,26 +79,28 @@ class Camera:
     def read(self) -> np.ndarray:
         try:
             return self._read()
-        except IOError as e:
-            print("ERROR ", end='')
-            print("Camera: ", e)
+
         except Exception as e:
             print("ERROR ", end='')
             print("Camera: ", e)
 
     def _threaded_read(self) -> np.ndarray:
-        return self.buffer.get(block=True)
+        self._buffer_semaphore.acquire()
+        frame =  self.buffer.popleft()
+        return frame
 
     def _sequential_read(self) -> np.ndarray:
-        return self.capture_frame()
+        frame = self.capture_frame()
+        return frame
 
     def _load_params(self) -> None:
         """ Loads the parameters of the camera from a json """
         params = self.json_handler.read(self.params_file_name)
 
         """ mapx and mapy are the matrix with the lens correction map """
-        self.mapx = np.asarray(params['matrix_x']).astype("float32")
-        self.mapy = np.asarray(params['matrix_y']).astype("float32")
+        mapx = np.asarray(params['matrix_x']).astype("float32")
+        mapy = np.asarray(params['matrix_y']).astype("float32")
+        self.mapx, self.mapy = cv2.convertMaps(mapx, mapy, cv2.CV_16SC2)
 
         """ The frame width and height """
         self.frame_width = int(params['default_frame_width'])
@@ -99,7 +110,8 @@ class Camera:
         self.camera_matrix = np.asarray(params['cam_matrix'])
         self.dist_vector = np.asarray(params['dist_vector'])
 
-    def set_frame_size(self, width: int, height: int) -> None:
+    def set_device(self, width: int, height: int) -> None:
+        self.capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
