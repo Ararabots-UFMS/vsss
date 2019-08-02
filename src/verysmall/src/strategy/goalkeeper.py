bfe8f9039b35b96959929aa851ffd9b55037f208
viniciosfaustino <vinicioskf@gmail.com>
2019-08-02 12:29:46 -0400

    - adicionando arvore de stopped - adicionando base tree que ja incorpora stopped, penalty e freeball - alterando arvore do goleiro para o modelo de basetree - alterando alterando insercoes de nos de children.append() para a funcao add_child()

--- src/verysmall/src/strategy/goalkeeper.py
+++ src/verysmall/src/strategy/goalkeeper.py
@@ -1,20 +1,13 @@
 from strategy.behaviour import *
 from strategy.actions.state_behaviours import InState
 from strategy.actions.movement_behaviours import GoToGoalCenter, StopAction
-from strategy.base_trees import Penalty, FreeBall
+from strategy.base_trees import Penalty, FreeBall, BaseTree
 from strategy.strategy_utils import GameStates
 
-class GoalKeeper(Selector):
+
+class GoalKeeper(BaseTree):
     def __init__(self, name: str = "behave"):
         super().__init__(name)
-        
-        stopped = Sequence('Stopped')
-        stopped.add_child(InState('CheckStoppedState', GameStates.STOPPED))
-        stopped.add_child(StopAction('Wait'))
-        self.add_child(stopped)
-        
-        self.add_child(Penalty())
-        self.add_child(FreeBall())
 
         normal = Sequence("Normal")
         normal.add_child(InState("CheckNormalState", GameStates.NORMAL))
