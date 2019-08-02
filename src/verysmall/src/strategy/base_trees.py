adc314a2e8d186e8c659db7e4865bbc315b427db
viniciosfaustino <vinicioskf@gmail.com>
2019-08-02 12:29:46 -0400

    - adicionando arvore de stopped - adicionando base tree que ja incorpora stopped, penalty e freeball - alterando arvore do goleiro para o modelo de basetree - alterando alterando insercoes de nos de children.append() para a funcao add_child()

--- src/verysmall/src/strategy/base_trees.py
+++ src/verysmall/src/strategy/base_trees.py
@@ -2,7 +2,7 @@ from strategy.behaviour import BlackBoard, Sequence, Selector, TaskStatus
 from strategy.actions.state_behaviours import InState, ChangeState
 from strategy.actions.game_behaviours import IsBehindBall
 from strategy.actions.movement_behaviours import ChargeWithBall, univectorField, GoToBallUsingUnivector, \
-    GoToAttackGoalUsingUnivector
+    GoToAttackGoalUsingUnivector, StopAction
 from strategy.strategy_utils import GameStates
 
 
@@ -11,20 +11,20 @@ class Penalty(Sequence):
         super().__init__(name)
 
         check_state = InState('CheckPenaltyState', GameStates.PENALTY)
-        self.children.append(check_state)
+        self.add_child(check_state)
 
         check_if_behind_ball = Selector("CheckIfBehindBall")
         is_behind = IsBehindBall("Check", 25)
         change_state = ChangeState("ReturnToNormal", GameStates.NORMAL)
 
-        check_if_behind_ball.children.append(is_behind)
-        check_if_behind_ball.children.append(change_state)
+        check_if_behind_ball.add_child(is_behind)
+        check_if_behind_ball.add_child(change_state)
 
-        self.children.append(check_if_behind_ball)
+        self.add_child(check_if_behind_ball)
 
         charge_with_ball = GoToAttackGoalUsingUnivector('FollowGoal', acceptance_radius=5, speed_prediction=False) #ChargeWithBall("ChargeWithBall", max_speed=220)
 
-        self.children.append(charge_with_ball)
+        self.add_child(charge_with_ball)
 
 
 class FreeBall(Sequence):
@@ -32,15 +32,32 @@ class FreeBall(Sequence):
         super().__init__(name)
 
         check_state = InState('CheckFreeBallState', GameStates.FREE_BALL)
-        self.children.append(check_state)
+        self.add_child(check_state)
 
         check_if_behind_ball = Selector("CheckIfBehindBall")
         is_behind = IsBehindBall("Check", 25)
         change_state = ChangeState("ReturnToNormal", GameStates.NORMAL)
 
-        check_if_behind_ball.children.append(is_behind)
-        check_if_behind_ball.children.append(change_state)
+        check_if_behind_ball.add_child(is_behind)
+        check_if_behind_ball.add_child(change_state)
 
-        self.children.append(check_if_behind_ball)
-        self.children.append(GoToBallUsingUnivector('FollowBall'))  # FollowBall
-        self.children.append(GoToAttackGoalUsingUnivector('FollowGoal'))  # FollowBall
+        self.add_child(check_if_behind_ball)
+        self.add_child(GoToBallUsingUnivector('FollowBall'))  # FollowBall
+        self.add_child(GoToAttackGoalUsingUnivector('FollowGoal'))  # FollowBall
+
+
+class Stopped(Sequence):
+    def __init__(self, name: str = 'Stopped'):
+        super().__init__(name)
+
+        self.add_child(InState('CheckStoppedState', GameStates.STOPPED))
+        self.add_child(StopAction('Wait'))
+
+
+class BaseTree(Selector):
+    def __init__(self, name: str = "BaseTree"):
+        super.__init__(name)
+
+        self.add_child(Stopped('Stopped'))
+        self.add_child(Penalty('Penalty'))
+        self.add_child(FreeBall('FreeBall'))
