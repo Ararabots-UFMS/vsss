a79c140ee11577da0362cdc197e453d0c91cc5a5
MarquesMan <marquesnavarezi@hotmail.com>
2019-08-20 13:03:31 -0400

    Alteracoes no controle e na sigmoid

--- src/verysmall/src/robot_module/control.py
+++ src/verysmall/src/robot_module/control.py
@@ -1,42 +1,40 @@
-from typing import Tuple, List
-import numpy as np
 import math
 from bisect import bisect_left
 from time import time
-#import robot_module.robot as robot
+from typing import Tuple, List
+import numpy as np
+
+import utils.math_utils as mth
 from robot_module.PID import PIDController
 from robot_module.movement.definitions import OpCodes
-
 from utils.math_utils import RAD2DEG, DEG2RAD, FORWARD, BACKWARDS
-import utils.math_utils as mth 
-
-import rospy
 
 Constants = Tuple[int, float, float, float]
 
+
 class Control:
     def __init__(self, myrobot,
-                       constants: List[Constants],
-                       max_fine_movment_speed) -> None:
+                 constants: List[Constants],
+                 max_fine_movment_speed) -> None:
         self._myrobot = myrobot
         self._max_fine_movment_speed = max_fine_movment_speed
-        self._alpha = 15 # centimeters
+        self._alpha = 15  # centimeters
 
         self._head = FORWARD
         self._hysteresis_angle_window = 20 * DEG2RAD
-        self._upper_angle_tol = math.pi/2.0 + self._hysteresis_angle_window
-        self._lower_angle_tol = math.pi/2.0 - self._hysteresis_angle_window
+        self._upper_angle_tol = math.pi / 2.0 + self._hysteresis_angle_window
+        self._lower_angle_tol = math.pi / 2.0 - self._hysteresis_angle_window
 
         self._pid_constants_set = sorted(constants)
-        self._speed_keys = [s[0] for s in self._pid_constants_set] # TODO: get a better name
+        self._speed_keys = [s[0] for s in self._pid_constants_set]  # TODO: get a better name
         self._pidController = PIDController()
         self._pid_last_use = time()
-        self._pid_reset_time = 0.032 # 2 frames
+        self._pid_reset_time = 0.032  # 2 frames
 
     def get_wheels_speeds(self, opcode: OpCodes,
-                                angle: float,
-                                speed: int,
-                                distance: float) -> Tuple[float, float]:
+                          angle: float,
+                          speed: int,
+                          distance: float) -> Tuple[float, float]:
 
         if opcode == OpCodes.NORMAL:
             return self._follow_vector(speed, angle, distance)
@@ -46,23 +44,24 @@ class Control:
             return 255, -255
         else:
             return 0, 0
-    
-    def _follow_vector(self, speed: int, 
-                             angle: float, 
-                             distance: float,
-                             optimal_speed: bool = True) -> Tuple[float, float]:      
+
+    def _follow_vector(self, speed: int,
+                       angle: float,
+                       distance: float,
+                       optimal_speed: bool = True) -> Tuple[float, float]:
         self.set_head(angle)
 
         diff_angle = self.get_diff_angle(angle)
-        
-        if optimal_speed == True:
+
+        if optimal_speed:
             speed = self.get_optimal_speed(speed, diff_angle, distance)
+            speed = min(speed, self._myrobot.get_next_speed())
 
         t = time()
         if t - self._pid_last_use > self._pid_reset_time:
             self._pidController.reset()
         self._pid_last_use = t
-        
+
         constants = self.interpolate_constants(speed)
 
         self._pidController.set_constants(*constants)
@@ -72,7 +71,6 @@ class Control:
             return speed + correction, speed - correction
         else:
             return -speed + correction, -speed - correction
-        
 
     def set_head(self, angle: float) -> np.array:
         abs_diff = abs(mth.min_angle(self._myrobot.orientation, angle))
@@ -80,50 +78,50 @@ class Control:
             self._head = BACKWARDS
         elif abs_diff < self._lower_angle_tol:
             self._head = FORWARD
-    
+
     def get_diff_angle(self, target_angle: float) -> float:
         if self._head == FORWARD:
             orientation = self._myrobot.orientation
         else:
             orientation = mth.wrap2pi(self._myrobot.orientation + math.pi)
-        
+
         return mth.min_angle(orientation, target_angle)
-    
+
     def interpolate_constants(self, speed: float) -> Tuple[float, float, float]:
         i = bisect_left(self._speed_keys, speed)
-        
+
         if i == len(self._pid_constants_set):
             i -= 1
             return self._pid_constants_set[i][1:]
-        
+
         if self._pid_constants_set[i][0] == speed:
             return self._pid_constants_set[i][1:]
-        
+
         if 0 < i < len(self._pid_constants_set):
-            w1 = speed - self._pid_constants_set[i-1][0]
-            set1 = np.array(self._pid_constants_set[i-1][1:])
-            
+            w1 = speed - self._pid_constants_set[i - 1][0]
+            set1 = np.array(self._pid_constants_set[i - 1][1:])
+
             w2 = self._pid_constants_set[i][0] - speed
             set2 = np.array(self._pid_constants_set[i][1:])
-            
-            norm = self._pid_constants_set[i][0] - self._pid_constants_set[i-1][0]
-            return (w1*set1 + w2*set2) / norm        
-    
+
+            norm = self._pid_constants_set[i][0] - self._pid_constants_set[i - 1][0]
+            return (w1 * set1 + w2 * set2) / norm
+
     def get_optimal_speed(self, target_speed: float,
-                                diff_angle: float, 
-                                target_distance: float) -> float:
-        
+                          diff_angle: float,
+                          target_distance: float) -> float:
+
         if target_speed < self._max_fine_movment_speed:
             return target_speed
-        elif diff_angle*RAD2DEG < 10 and target_distance > 2*self._alpha:
+        elif diff_angle * RAD2DEG < 10 and target_distance > 2 * self._alpha:
             return target_speed
         else:
             optimal_speed = self.sigmoid(target_speed, target_distance)
             return optimal_speed
-    
+
     def sigmoid(self, target_speed: float,
-                      distance: float) -> float:
+                distance: float) -> float:
         scale = target_speed - self._max_fine_movment_speed
 
-        s = scale / (1 + math.exp(0.5*(-distance + self._alpha))) 
+        s = scale / (1 + math.exp(0.5 * (-distance + self._alpha * 1.5)))
         return s + self._max_fine_movment_speed
--- src/verysmall/src/strategy/goalkeeper.py
+++ src/verysmall/src/strategy/goalkeeper.py
@@ -1,6 +1,6 @@
 from strategy.behaviour import *
 from strategy.actions.state_behaviours import InState
-from strategy.actions.movement_behaviours import GoToGoalCenter, StopAction, AlignWithAxis
+from strategy.actions.movement_behaviours import GoToGoalCenter, StopAction, AlignWithAxis, MarkBallOnAxis
 from strategy.base_trees import Penalty, FreeBall, BaseTree
 from strategy.strategy_utils import GameStates
 
@@ -11,6 +11,7 @@ class GoalKeeper(BaseTree):
 
         normal = Sequence("Normal")
         normal.add_child(InState("CheckNormalState", GameStates.NORMAL))
-        normal.add_child(GoToGoalCenter(max_speed=120, acceptance_radius=3))
+        #normal.add_child(GoToGoalCenter(max_speed=120, acceptance_radius=3))
         normal.add_child(AlignWithAxis())
+        normal.add_child(MarkBallOnAxis(max_speed=200, acceptance_radius=5, clamp_min=35.0, clamp_max=90.0))
         self.add_child(normal)
