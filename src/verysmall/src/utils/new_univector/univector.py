1c94125ee82fe021e5af054371946abb976b4f11
MarquesMan <marquesnavarezi@hotmail.com>
2019-08-28 16:01:19 -0400

    Guerra de constantes

--- src/verysmall/src/utils/new_univector/univector.py
+++ src/verysmall/src/utils/new_univector/univector.py
@@ -2,44 +2,50 @@ from math import pi, sqrt, cos, sin, atan2
 import utils.new_univector.math_utils as math_utils
 import numpy as np
 
-de = 12#5.37
+de =  12#5.37
 kr = 6
 ko = 40
 d_min = 8.5
 delta = 4.5
 
+
 def v_obs():
     return np.array([0, 0])
+
 
 def v_robot():
     return np.array([0, 0])
 
-def phiH(rho, theta, ccw = True, de = de, kr = kr):
+
+def phiH(rho, theta, ccw=True, de=de, kr=kr):
     if rho > de:
         angle = (pi / 2) * (2 - ((de + kr) / (rho + kr)))
     elif 0 <= rho <= de:
         angle = (pi / 2) * sqrt(rho / de)
-    
+
     if ccw:
         return math_utils.wrapToPi(theta + angle)
     else:
         return math_utils.wrapToPi(theta - angle)
-    
+
+
 def Nh(phi):
     return np.array([cos(phi), sin(phi)])
 
-def phiR(d_x, d_y): # Theta
+
+def phiR(d_x, d_y):  # Theta
     return math_utils.wrapToPi(atan2(d_y, d_x))
 
-def phiTuf(theta, d_x, d_y, radius = de):
+
+def phiTuf(theta, d_x, d_y, radius=de):
     y_l = d_y + radius
     y_r = d_y - radius
 
     rho_l = math_utils.norm(d_x, d_y - radius)
     rho_r = math_utils.norm(d_x, d_y + radius)
 
-    phi_ccw = phiH(rho_l, theta, ccw = False)
-    phi_cw = phiH(rho_r, theta, ccw = True)
+    phi_ccw = phiH(rho_l, theta, ccw=False)
+    phi_cw = phiH(rho_r, theta, ccw=True)
 
     nh_ccw = Nh(phi_ccw)
     nh_cw = Nh(phi_cw)
@@ -49,13 +55,14 @@ def phiTuf(theta, d_x, d_y, radius = de):
     if -radius <= d_y < radius:
         phi_tuf = atan2(spiral_merge[1], spiral_merge[0])
     elif d_y < -radius:
-        phi_tuf = phiH(rho_l, theta, ccw = True)
+        phi_tuf = phiH(rho_l, theta, ccw=True)
     else:
-        phi_tuf = phiH(rho_r, theta, ccw = False)
+        phi_tuf = phiH(rho_r, theta, ccw=False)
 
     return math_utils.wrapToPi(phi_tuf)
 
-def phiAuf(obs_x, obs_y, r_x, r_y, obs_robot_dist, v_obs = v_obs(), v_robot = v_robot(), ko = ko):
+
+def phiAuf(obs_x, obs_y, r_x, r_y, obs_robot_dist, v_obs=v_obs(), v_robot=v_robot(), ko=ko):
     obstacle_position = np.array([obs_x, obs_y])
     s_vec = ko * (v_obs - v_robot)
     s_norm = math_utils.norm(s_vec[0], s_vec[1])
@@ -71,11 +78,11 @@ def phiAuf(obs_x, obs_y, r_x, r_y, obs_robot_dist, v_obs = v_obs(), v_robot = v_
 
     return math_utils.wrapToPi(phi_auf)
 
-def phiComposed(phi_tuf, phi_auf, R, obstacle, delta = delta, d_min = d_min):
-    
+
+def phiComposed(phi_tuf, phi_auf, R, obstacle, delta=delta, d_min=d_min):
     if obstacle is None:
         phi_composed = math_utils.wrapToPi(phi_tuf)
-    
+
     else:
         gauss = math_utils.gaussian(R - d_min, delta)
 
@@ -83,14 +90,14 @@ def phiComposed(phi_tuf, phi_auf, R, obstacle, delta = delta, d_min = d_min):
             phi_composed = phi_auf
         else:
             diff = math_utils.wrapToPi(phi_auf - phi_tuf)
-            phi_composed = math_utils.wrapToPi(gauss *  diff + phi_tuf)
+            phi_composed = math_utils.wrapToPi(gauss * diff + phi_tuf)
 
     return math_utils.wrapToPi(phi_composed)
 
-def generateUnivectorField(r_x, r_y, ball_pos, obs_pos, de = de, v_obs =
-        np.array([0,0]), v_rob: list = np.array([0,0]), ko: float = ko, delta: 
-        float = delta, d_min: float = d_min) -> float:
 
+def generateUnivectorField(r_x, r_y, ball_pos, obs_pos, de=de, v_obs=
+np.array([0, 0]), v_rob: list = np.array([0, 0]), ko: float = ko, delta:
+float = delta, d_min: float = d_min) -> float:
     ball_x, ball_y = ball_pos
     d_ball_x, d_ball_y = math_utils.delta_axis(ball_x, ball_y, r_x, r_y)
     theta = phiR(d_ball_x, d_ball_y)
@@ -102,9 +109,8 @@ def generateUnivectorField(r_x, r_y, ball_pos, obs_pos, de = de, v_obs =
     robot_obs_x, robot_obs_y = math_utils.delta_axis(obs_x, obs_y, r_x, r_y)
     R = math_utils.norm(robot_obs_x, robot_obs_y)
     robot_obs_dist = math_utils.norm(robot_obs_x, robot_obs_y)
-    
+
     phi_auf = phiAuf(obs_x, obs_y, r_x, r_y, robot_obs_dist, v_obs, v_rob, ko)
     phi_composed = phiComposed(phi_tuf, phi_auf, R, obstacle, delta, d_min)
 
     return Nh(phi_composed)
-
--- src/verysmall/src/utils/univector_tester.py
+++ src/verysmall/src/utils/univector_tester.py
@@ -4,7 +4,28 @@ import time
 from utils.new_univector import univector
 from utils.json_handler import JsonHandler
 from robot_module.movement.univector.un_field import UnivectorField
+from strategy.arena_utils import MAX_W_SIZE, MAX_H_SIZE
+from robot_module.movement.univector.debug import enemyColor, teamColor
 import cv2
+
+enemies = [[int(0.25 * MAX_W_SIZE), int(0.75 * MAX_H_SIZE)],
+           [int(0.5 * MAX_W_SIZE), int(0.25 * MAX_H_SIZE)],
+           [int(0.75 * MAX_W_SIZE), int(0.75 * MAX_H_SIZE)]
+           ]
+enemies_speed = np.array([[0, 0] for _ in range(len(enemies))])
+
+
+def drawRobot(img, robotPos, enemy=False):
+    if enemy:
+        color = enemyColor
+    else:
+        color = teamColor
+
+    pos = cm2pixel([robotPos[0], robotPos[1]])
+    topLeft = (pos[0] - 15, pos[1] - 15)
+    bottomRight = (pos[0] + 15, pos[1] + 15)
+    cv2.rectangle(img, topLeft, bottomRight, color, -1)
+
 
 def read_file():
     try:
@@ -12,16 +33,23 @@ def read_file():
     except IOError:
         content = []
 
-        content = np.array([[[randint(0, 150), randint(0, 130)], [randint(0, 150), randint(0, 130)]] for _ in range(1000)])
+        content = np.array(
+            [[[randint(0, 150), randint(0, 130)], [randint(0, 150), randint(0, 130)]] for _ in range(10000)])
         np.save("univector_rand_points.npy", content)
-        
+
         return content
+
+
+def drawBall(img, ballPos):
+    cv2.circle(img, (ballPos[0], ballPos[1]), 9, (0, 69, 255), -1)
+
 
 def cm2pixel(pos):
     posArray = np.array(pos)
-    return 4*posArray
+    return 4 * posArray
 
-def drawPath(img, start, end, univetField, new_uni = False):
+
+def drawPath(img, start, end, univetField, new_uni=False, should_draw=False):
     currentPos = start
     _currentPos = cm2pixel(currentPos)
 
@@ -31,26 +59,27 @@ def drawPath(img, start, end, univetField, new_uni = False):
 
     t0 = time.time()
 
-    while(np.linalg.norm(currentPos - end) >= beta):
-        
+    while np.linalg.norm(currentPos - end) >= beta:
+
         if new_uni:
-            v = univector.generateUnivectorField(currentPos[0], currentPos[1], end, [[1000, 1000]])
+            v = univector.generateUnivectorField(currentPos[0], currentPos[1], end, enemies)
         else:
-            angle = univetField.get_angle_vec(currentPos,  np.array([0,0]), end, np.array([-1.0,0.0]))
+            angle = univetField.get_angle_vec(currentPos, np.array([0, 0]), end, np.array([-1.0, 0.0]))
             v = [np.cos(angle), np.sin(angle)]
 
-        newPos = currentPos + (alpha*np.array(v))
+        newPos = currentPos + (alpha * np.array(v))
         _newPos = cm2pixel(newPos).astype(int)
 
-        if new_uni:
-            cv2.line(img, (_currentPos[0], _currentPos[1]), (_newPos[0], _newPos[1]), (255,255,255), 3)
-        else:
-            cv2.line(img, (_currentPos[0], _currentPos[1]), (_newPos[0], _newPos[1]), (255,255,0), 3)
+        if should_draw:
+            if new_uni:
+                cv2.line(img, (_currentPos[0], _currentPos[1]), (_newPos[0], _newPos[1]), (255, 255, 255), 3)
+            else:
+                cv2.line(img, (_currentPos[0], _currentPos[1]), (_newPos[0], _newPos[1]), (255, 255, 0), 3)
 
-        cv2.imshow('field', img)
-        cv2.waitKey(1)
+            cv2.imshow('field', img)
+            cv2.waitKey(1)
 
-        if (time.time() - t0 > 5):
+        if (time.time() - t0) > 0.5:
             return False, newPos
 
         currentPos = newPos
@@ -60,10 +89,11 @@ def drawPath(img, start, end, univetField, new_uni = False):
 
 if __name__ == "__main__":
     imgField = cv2.imread('../robot_module/movement/univector/img/vss-field.jpg')
-     
+
     content = read_file()
+    # content = np.array([[[100, 75], [int(0.05 * MAX_W_SIZE), int(0.75 * MAX_H_SIZE)]]])
     univector_list = JsonHandler().read("parameters/univector_constants.json")
-    
+
     # Creates the univector field
     univetField = UnivectorField()
     RADIUS = univector_list['RADIUS']
@@ -72,14 +102,34 @@ if __name__ == "__main__":
     DMIN = univector_list['DMIN']
     LDELTA = univector_list['LDELTA']
     univetField.update_constants(RADIUS, KR, K0, DMIN, LDELTA)
-    univetField.update_obstacles([[1000, 1000]],[[0.0, 0.0]])
+    univetField.update_obstacles(enemies, enemies_speed)
+
+    juan_vector = old_vector = total_points = 0
+    should_draw = False
 
     for point in content:
-
         imgField2 = np.copy(imgField)
 
-        drawPath(imgField2,point[0], point[1], univetField)
-        drawPath(imgField2,point[0], point[1], None, True)
-        
-        #cv2.imshow('field', imgField2)
-        cv2.waitKey(0)
+        if should_draw:
+            drawBall(imgField2, cm2pixel(point[1]))
+            for enemy_pos in enemies:
+                drawRobot(imgField2, enemy_pos, True)
+
+        result, _ = drawPath(imgField2, point[0], point[1], univetField, False, should_draw)
+        old_vector += result
+
+        result, _ = drawPath(imgField2, point[0], point[1], None, True, should_draw)
+        juan_vector += result
+
+        # cv2.imshow('field', imgField2)
+        if should_draw:
+            cv2.waitKey(0)
+
+        total_points += 1
+
+        if total_points % 100 == 0:
+            print("Old Vector Success Rate: " + str(old_vector / total_points))
+            print("Juan Vector Success Rate: " + str(juan_vector / total_points))
+
+    print("Old Vector Success Rate: " + str(old_vector / total_points))
+    print("Juan Vector Success Rate: " + str(juan_vector / total_points))
