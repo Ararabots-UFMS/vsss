411b4a0941157b745b138a1fcc790396e6fedea7
Lucas <marquesnavarezi@hotmail.com>
2020-06-15 20:21:58 -0400

    Updated ros install and created install-utils

--- scripts/install-ros.sh
+++ scripts/install-ros.sh
@@ -1,18 +1,31 @@
+#!/bin/bash
 # setup sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # setup keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
+echo "Please select ROS version:"
+
+select os in kinetic melodic noetic
+do
+case $os in "kinetic"|"melodic"|"noetic")
 # install
 sudo apt-get update
-sudo apt-get install ros-melodic-ros-base
+sudo apt-get install ros-$os-ros-base
 sudo rosdep init
 rosdep update
 
 # environment setup
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/$os/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+;;
+# Matching with invalid data
+*)
+echo "Invalid entry."
+break
+esac
+done
 
 # dependencies for building packages
 sudo apt install python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
---
+++ scripts/install-utils.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install libbluetooth-dev -y
+sudo apt install qv4l2 -y
+
+source ./install-ros.sh
+source ./install-fltk.sh
+source ./install-opencv.sh
